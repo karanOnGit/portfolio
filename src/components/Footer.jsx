84253c124ea544c3194334258547bce244098d2f
Karan Bhardwaj <karanbhardwaj1107@gmail.com>
2026-09-14 18:27:32 +0530

    upodated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 export default function Footer() {
     const year = new Date().getFullYear()
 
@@ -5,6 +7,31 @@ export default function Footer() {
         <footer className="footer">
             <p>© {year} Karan Bhardwaj. All rights reserved.</p>
             <div className="footer__links">
+                <Link to="/blog" className="footer__link">
+                    Blog
+                </Link>
+                <a
+                    href="https://github.com/karanongit"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="footer__link"
+                >
+                    GitHub
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/karan-bhardwaj-849296227/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="footer__link"
+                >
+                    LinkedIn
+                </a>
+                <a
+                    href="mailto:karanbhardwaj1107@gmail.com"
+                    className="footer__link"
+                >
+                    Email
+                </a>
             </div>
         </footer>
     )
